fd47784705a167a58b811f1e7518d9631c2cf878
yhb <yanghb2019@lzu.edu.cn>
2023-10-18 11:39:13 +0800

    update memory.c

--- code11/kernel/mm/memory.c
+++ code11/kernel/mm/memory.c
@@ -7,9 +7,8 @@
 #define CSR_DMW0_PLV0 (1UL << 0)
 #define MEMORY_SIZE 0x8000000
 #define NR_PAGE (MEMORY_SIZE >> 12)
-#define RAM_BASE 0x90000000UL
-#define START_FRAME (RAM_BASE >> 12)
-#define NR_PAGE (MEMORY_SIZE >> 12)
+#define KERNEL_START_PAGE (0x200000UL >> 12)
+#define KERNEL_END_PAGE (0x300000UL >> 12)
 #define ENTRY_SIZE 8
 #define PWCL_PTBASE 12
 #define PWCL_PTWIDTH 9
@@ -27,12 +26,12 @@ unsigned long get_page()
 	unsigned long page;
 	unsigned long i;
 
-	for (i = 0; i < NR_PAGE; i++)
+	for (i = NR_PAGE - 1; i >= 0; i--)
 	{
 		if (mem_map[i] != 0)
 			continue;
 		mem_map[i] = 1;
-		page = ((START_FRAME + i) << 12) | DMW_MASK;
+		page = (i << 12) | DMW_MASK;
 		set_mem((char *)page, 0, PAGE_SIZE);
 		return page;
 	}
@@ -43,7 +42,7 @@ void free_page(unsigned long page)
 {
 	unsigned long i;
 
-	i = ((page & ~DMW_MASK) >> 12) - START_FRAME;
+	i = (page & ~DMW_MASK) >> 12;
 	if (!mem_map[i])
 		panic("panic: try to free free page!\n");
 	mem_map[i]--;
@@ -52,7 +51,7 @@ void share_page(unsigned long page)
 {
 	unsigned long i;
 
-	i = ((page & ~DMW_MASK) >> 12) - START_FRAME;
+	i = (page & ~DMW_MASK) >> 12;
 	if (!mem_map[i])
 		panic("panic: try to share free page!\n");
 	mem_map[i]++;
@@ -61,7 +60,7 @@ int is_share_page(unsigned long page)
 {
 	unsigned long i;
 
-	i = ((page & ~DMW_MASK) >> 12) - START_FRAME;
+	i = (page & ~DMW_MASK) >> 12;
 	if (mem_map[i] > 1)
 		return 1;
 	else
@@ -202,7 +201,12 @@ void mem_init()
 	int i;
 
 	for (i = 0; i < NR_PAGE; i++)
-		mem_map[i] = 0;
+	{
+		if (i >= KERNEL_START_PAGE && i < KERNEL_END_PAGE)
+			mem_map[i] = 100;
+		else
+			mem_map[i] = 0;
+	}
 	write_csr_64(CSR_DMW0_PLV0 | DMW_MASK, CSR_DMW0);
 	write_csr_64(0, CSR_DMW3);
 	write_csr_64((PWCL_EWIDTH << 30) | (PWCL_PDWIDTH << 15) | (PWCL_PDBASE << 10) | (PWCL_PTWIDTH << 5) | (PWCL_PTBASE << 0), CSR_PWCL);
